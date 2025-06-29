5ee6ea42f00baec214b9540b430a961f5821fe35
Sirine hjaij <hjaijsirine@gmail.com>
2025-06-29 04:27:35 +0100

    added TokenObtainPairView and TokenRefreshView

--- EcoTrajet/api/urls.py
+++ EcoTrajet/api/urls.py
@@ -3,11 +3,16 @@ from .views import main
 from rest_framework.authtoken.views import obtain_auth_token
 from apps.users.views import ProfileView
 from apps.users.views import ProfileView, UserCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='api-login'),
-    path('profile/', ProfileView.as_view(), name='api-profile'),
-    path('register/', UserCreateView.as_view(), name='api-register'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('login/', obtain_auth_token, name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('register/', UserCreateView.as_view(), name='register'),
 
 ]
